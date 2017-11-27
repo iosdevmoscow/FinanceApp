b8160b6793570e216351496386145bf9b8ca4d7b
Kirill Khudyakov <hudiakov@gmail.com>
2017-11-27 17:28:23 +0300

    init Xcode project

---
+++ FinanceApp/FinanceApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  FinanceApp
+//
+//  Created by Kirill Khudyakov on 27.11.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+  
+        return true
+    }
+
+}
+
---
+++ FinanceApp/FinanceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceApp
+//
+//  Created by Kirill Khudyakov on 27.11.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
